1e5e788ca1a27aaddfca39f710fcc6a480d8e7ce
rennspigelmyer <catherinespigelmyer95@gmail.com>
2021-09-20 16:43:46 -0400

    Completed initial JS Basics Exercise

---
+++ script.js
@@ -0,0 +1,63 @@
+// JavaScript Basics Exercise
+
+// 1
+console.log(45);
+
+//2
+// The 5 main data types are Numbers, Strings, Booleans, Null, Undefined
+
+//3
+//The 6 operator symbols are: 
+/* 
+Addition: +
+Subtraction: -
+Multiplication: *
+Division: /
+Exponentiation: **
+Modules: %
+*/
+
+//4 
+console.log(10%3);
+
+//5
+console.log('I\'M MARRY POPPINS, YA\'LL');
+console.log('I\'M MARRY POPPINS, YA\'LL'.length);
+
+//6 
+console.log("Hawk" + "Eye");
+
+
+//7
+console.log("Renn".length + "Spigelmyer".length); 
+
+//8
+console.log("Avengers Endgame"[15]);
+
+
+//BONUS
+
+//1
+console.log('I\'m'[1]); // Confused. I thought backslashes don't show up in terminals.
+
+//2
+console.log(5**4-9);
+
+//3
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+
+//4 
+console.log("CAP"[1])
+console.log("VISION"[0])
+console.log("OKOYE"[4])
+console.log("NICK FURY"[0])
+console.log("GROOT"[0])
+console.log("DOCTOR STRANGE"[5])
+console.log("THOR"[3])
+console.log("STAR-LORD"[0])
+
+//5 
+var m=25;
+var c=4;
+var u=m+c;
+console.log(u);
